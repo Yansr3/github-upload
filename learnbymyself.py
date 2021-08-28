b02f3ad98880438a0714d439df3e62718fb7fbd8
Yansr3 <89668322+Yansr3@users.noreply.github.com>
2021-08-27 20:38:41 -0400

    Add files via upload

---
+++ learnbymyself.py
@@ -0,0 +1,17 @@
+print("Hello World")
+print(3+5)
+print(3*7)
+print(2**3)
+print(13%4)
+
+#py -m pip install numpy
+
+import numpy as np
+print(np.array([1,2,3]))
+
+from numpy import array
+print(array([4,5,6]))
+
+import math
+print(math.pi)
+
